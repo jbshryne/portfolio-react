d559e6a58a914b3e9259c03fa9629e697cb9a7ef
Jon Brennan <jonbrennan@Jons-MacBook-Pro.local>
2023-09-23 10:55:37 -0400

    fixing backend url

--- src/loaders.js
+++ src/loaders.js
@@ -1,5 +1,9 @@
-// const URL = "https://jpb-profile-backend.onrender.com";
-const URL = "http://localhost:4000";
+const URL = "https://jpb-profile-backend.onrender.com";
+// const URL = "http://localhost:4000";
+// import dotenv from "dotenv";
+// dotenv.config();
+
+// const URL = process.env.API_URL;
 
 export const projectsLoader = async () => {
   const response = await fetch(URL + "/projects");
